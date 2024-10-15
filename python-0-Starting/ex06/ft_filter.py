12f2872209f231635106c616cd760dabc31616bb
ahlyel-amine <ahlyel-amine@users.noreply.github.com>
2024-10-15 09:33:27 +0100

    Refactor ft_filter function to use yield instead of list comprehension

--- python-0-Starting/ex06/ft_filter.py
+++ python-0-Starting/ex06/ft_filter.py
@@ -1,9 +1,9 @@
 def ft_filter(function , iterable) -> filter:
     """
-        ft_filter(function or None, iterable) --> filter object
+ft_filter(function or None, iterable) --> filter object
 
-        Return an iterator yielding those items of iterable for which function(item)
-        is true. If function is None, return the items that are true.
+Return an iterator yielding those items of iterable for which function(item)
+is true. If function is None, return the items that are true.
     """
     if function is None:
         return ([item for item in iterable if item])
