f12e472ebf74c9b4bf1e4d7c9c25e5b62482e19c
ahlyel-amine <ahlyel-amine@users.noreply.github.com>
2024-10-15 09:33:55 +0100

    refactor: Update ft_filter function to use yield instead of list comprehension

--- python-0-Starting/ex06/filterstring.py
+++ python-0-Starting/ex06/filterstring.py
@@ -8,7 +8,7 @@ def main():
         assert sys.argv[2].isdigit(), "AssertionError: the arguments are bad"
         nbr = int(sys.argv[2])
         splited = sys.argv[1].split()
-        print(list(ft_filter(lambda item : len(item) > nbr and item.isalnum() , splited)))
+        print(ft_filter(lambda item : len(item) > nbr and item.isalnum() , splited))
 
     except AssertionError as error:
         print(error)
